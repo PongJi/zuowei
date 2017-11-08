ec8343a3d7a4b62370a9ffec6f08e47f1bbc5fc4
PongJi <31606613+PongJi@users.noreply.github.com>
2017-11-08 18:00:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,6 +25,86 @@ window.onload = function() {
 		var Oweekday = document.getElementsByClassName('week')[0];
 		Oweekday.innerHTML = weekday;
 		};
+	getNowFormatDate();
 
-	getNowFormatDate()
-}
+	//全部商品分类点击事件
+	var Oli = document.getElementsByClassName('goods-list')[0];
+	var Opic = document.getElementsByClassName('pic-show')[0];
+	var Ogoods = document.getElementsByClassName('goods-banner')[0];
+	var Ocontent = document.getElementsByClassName('content')[0];
+
+	Ogoods.style.display = 'none';
+
+	Oli.addEventListener('mouseenter',function(){	
+		if(Opic.style.display == 'none'){ 
+         	Opic.style.display = 'block';
+         	Ogoods.style.display = 'none';        	
+            Ocontent.style.marginTop = 24+'px';
+        }
+        else{
+            Opic.style.display = 'none';
+            Ogoods.style.display = 'block';
+            Ocontent.style.marginTop = 324+'px';
+        }
+	},false);
+};
+
+
+$(function(){
+	//banner图动画效果
+	var timer=null;
+	var index=0;
+
+	//右下角指示灯
+	$(".banner .light li").mouseover(function(){
+		index =$(this).index();//获取当前li的索引值
+		$(this).addClass("active").siblings().removeClass();
+		$(".banner .pic li").eq(index).fadeIn().siblings().fadeOut();
+	});
+
+	//下一页切换
+	$(".next").click(function(){
+		index++;
+		if(index>5){
+			index = 0;
+		}
+		$(".banner .light li").eq(index).addClass("active").siblings().removeClass();
+		$(".banner .pic li").eq(index).fadeIn().siblings().fadeOut();
+	}); 
+
+	//上一页切换
+	$(".prev").click(function(){
+		index--;
+		if(index<0){
+			index = 5;
+		}
+		$(".banner .light li").eq(index).addClass("active").siblings().removeClass();
+		$(".banner .pic li").eq(index).fadeIn().siblings().fadeOut();
+	}); 
+
+	//banner动画开始
+	function auto(){
+		timer = setInterval(function(){
+			index++;
+			if(index>5){
+				index = 0
+			};
+			$(".banner .light li").eq(index).addClass("active").siblings().removeClass();
+			$(".banner .pic li").eq(index).fadeIn().siblings().fadeOut();
+		},2000);
+
+	}
+
+	auto();
+	
+	//鼠标划入banner区域动画停止
+	$(".banner").mouseenter(function(){
+		// $(this).css({'cursor':'pointer'});
+		clearInterval(timer);
+	});
+
+	//鼠标划入banner区域动画继续
+	$(".banner").mouseleave(function(){
+		auto();
+	});
+});
